e30dabf917b1dbb5c084393834b6109716f76ea6
Milvio LuA <milviolua@gmail.com>
2020-06-13 02:31:37 +0200

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] grades = { 30, 430, 23, 10 };
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.Write(grades[i] + " ");
+            }
+        }
+        }
+    }
+}
